26148e8c3586393024f8333db600082b116f16ae
johanruizb <johan.andres.ruiz@correunivalle.edu.co>
2023-05-07 05:24:51 -0500

    gestionar colores

---
+++ src/theme/palette.jsx
@@ -0,0 +1,17 @@
+
+const palette = {
+  common: { black: '#000', white: '#fff' },
+  primary: {
+    main: '#555B6E',
+    contrastText: '#fff',
+  },
+  secondary: {
+    main: '#89B0AE',
+    contrastText: '#000',
+  },
+  background: {
+    paper: '#FAF9F9',
+  },
+};
+
+export default palette;

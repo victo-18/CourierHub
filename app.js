ba6a755f44fb407097ff59d9218f32e97d86699a
johanruizb <johan.andres.ruiz@correunivalle.edu.co>
2023-05-07 05:29:02 -0500

    gestionar cors

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var clientRouter = require('./routes/client');
+var courrierRouter = require('./routes/courrier');
+var loginRouter = require('./routes/login');
+
 const { sequelize } = require('./db/Models');
+const { insertDummy } = require('./db/dummy');
+const { authMiddleware } = require('./middleware/auth');
+const { clientMiddleware, adminMiddleware, courierMiddleware } = require('./middleware/roles');
 
 var app = express();
+
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,7 +31,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', authMiddleware, adminMiddleware, usersRouter);
+app.use('/clients', authMiddleware, clientMiddleware, clientRouter);
+app.use('/courriers', authMiddleware, courierMiddleware, courrierRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -45,6 +58,8 @@ app.use(function (err, req, res, next) {
     console.log('Connection has been established successfully.');
 
     // await sequelize.sync({ force: true });
+    insertDummy();
+
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }

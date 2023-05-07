36c9bf852ef69098cc73fc37dfc70ba6c66d1163
johanruizb <johan.andres.ruiz@correunivalle.edu.co>
2023-05-07 05:29:20 -0500

    poblar db

---
+++ db/dummy.js
@@ -0,0 +1,18 @@
+const { User, State, Country, City } = require("./Models");
+const fs = require('fs');
+
+async function insertDummy() {
+    // const countries = JSON.parse(fs.readFileSync('./db/countries.json'));
+    // const states = JSON.parse(fs.readFileSync('./db/states.json'));
+    // const cities = JSON.parse(fs.readFileSync('./db/cities.json'));
+    // const users = JSON.parse(fs.readFileSync('./db/users.json'));
+
+    // await Country.bulkCreate(countries);
+    // await State.bulkCreate(states);
+    // await City.bulkCreate(cities);
+    // await User.bulkCreate(users); // .KADI9{g#g9TvF},Nng6
+
+
+}
+
+module.exports = { insertDummy }

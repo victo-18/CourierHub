10b8ed87691c7fa11a2d0243b958459ee2920204
johanruizb <johan.andres.ruiz@correunivalle.edu.co>
2023-05-07 23:38:52 -0500

    mensajes mas apropiados

--- middleware/roles.js
+++ middleware/roles.js
@@ -4,29 +4,29 @@ const { User } = require('../db/Models');
 
 const adminMiddleware = async (req, res, next) => {
     // Obtener el objeto del usuario desde la base de datos
-    const user = await User.findOne({ email: req.user.email });
+    const user = await User.findOne({ where: { phone: req.user.phone } });
 
     // Comprobar si el usuario es un administrador
     if (user.role === 'ADMIN') next();
-    else return res.status(401).json({ message: 'No autorizado' });
+    else return res.status(401).json({ message: '¡Uups, no estas autorizado!', code: "UNAUTHORIZED" });
 };
 
 const clientMiddleware = async (req, res, next) => {
     // Obtener el objeto del usuario desde la base de datos
-    const user = await User.findOne({ email: req.user.email });
+    const user = await User.findOne({ where: { phone: req.user.phone } });
 
     // Comprobar si el usuario es un cliente
     if (user.role === 'CLIENT' || user.role === 'ADMIN') next();
-    else return res.status(401).json({ message: 'No autorizado' });
+    else return res.status(401).json({ message: '¡Uups, no estas autorizado!', code: "UNAUTHORIZED" });
 };
 
 const courierMiddleware = async (req, res, next) => {
     // Obtener el objeto del usuario desde la base de datos
-    const user = await User.findOne({ email: req.user.email });
+    const user = await User.findOne({ where: { phone: req.user.phone } });
 
     // Comprobar si el usuario es un mensajero
     if (user.role === 'COURIER' || user.role === 'ADMIN') next();
-    else return res.status(401).json({ message: 'No autorizado' });
+    else return res.status(401).json({ message: '¡Uups, no estas autorizado!', code: "UNAUTHORIZED" });
 };
 
 module.exports = { adminMiddleware, clientMiddleware, courierMiddleware };

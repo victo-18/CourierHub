66b23768a0076df95e7232f1d8d6d01c8c63f9f5
victo-18 <alomia18@gmail.com>
2023-05-19 00:01:11 -0500

    Inicnio desarrollo ventana administrador

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,42 +1,30 @@
-import * as React from 'react';
+//import * as React from 'react';
+//import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+//import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import DeliveryList from '../components/DeliveryList';
+import DeliveryDay from '../components/DeliveryDay';
 
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
+// const DeliveryDay = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#00000' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+// }));
 
 export default function AdminPage() {
   return (
-    <Card sx={{ minWidth: 275 }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2} columns={16}>
+        <Grid item xs={8}>
+         <DeliveryList/>
+        </Grid>
+        <Grid item xs={8}>
+         <DeliveryDay/>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }

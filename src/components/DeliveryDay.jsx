b0589e87798925f8a58afe487acf514a9da27f3d
victo-18 <alomia18@gmail.com>
2023-05-25 17:41:28 -0500

    dando estilos a tablas modulo admin

--- src/components/DeliveryDay.jsx
+++ src/components/DeliveryDay.jsx
@@ -7,16 +7,18 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Encabezado2 from '../utils/Encabezado2'
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+import '../styles/DeliveryDay.css'
+
+function createData(name, typeOfPackage, status, courier) {
+  return { name, typeOfPackage, status, courier };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Johan Andres Ruiz Bermudez','Electrodomestico','Entregado','Victor Alomia'),
+  createData('Victor Alfonso Alomia Angulo', 'Papeleria', 'No entregado', 'Camilo Ceballos'),
+  createData('Andres Felipe Morenno ', 'Cristaleria', 'Recogido', 'Cristian Presz'),
+  createData('Juan Sebatian Camino', 'Peresederos', 'Recogido', 'Andres Moreno'),
+  createData('Mauricio Merlano Cano','Delicado','No recogido','Cristian Mafla'),
 ];
 
 export default function DeliveryDay() {
@@ -25,12 +27,11 @@ export default function DeliveryDay() {
         <Encabezado2/>
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
         <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          <TableRow id='nombres'>
+            <TableCell>Nombre del cliente</TableCell>
+            <TableCell align="right">Numero y tipo de paquete</TableCell>
+            <TableCell align="right">Estado del paquete</TableCell>
+            <TableCell align="right">Nombre del mensajero</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -42,10 +43,10 @@ export default function DeliveryDay() {
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.typeOfPackage}</TableCell>
+              <TableCell align="right">{row.status}</TableCell>
+              <TableCell align="right">{row.courier}</TableCell>
+          
             </TableRow>
           ))}
         </TableBody>

4d85d29afecee580a17a685bd6911be2f675ef22
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2023-06-10 22:58:47 -0500

    añadir alias

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { manualChunks } from './src/utils/manualChunks'
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@styles": path.resolve(__dirname, "./src/styles/"),
+      "@components": path.resolve(__dirname, "./src/components/"),
+    },
+  },
   build: {
     rollupOptions: {
       output: {

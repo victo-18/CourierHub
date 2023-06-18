0b6f4cc7d76d1f99e5c21020727a89f26e4bb282
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2023-06-17 20:07:22 -0500

    añadir informacion del perfil

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { User } = require('../db/Models');
 var router = express.Router();
 const bcrypt = require('bcrypt');
 const { User } = require('../db/Models');
@@ -9,7 +10,7 @@ router.get('/', function (req, res, next) {
 });
 
 /* Obtiene todos los usuarios registrados en la BD*/
-router.get('/all', async (req, res) =>{
+router.get('/all', async (req, res) => {
   try {
     const response = await User.findAll();
     res.json(response);
@@ -19,7 +20,7 @@ router.get('/all', async (req, res) =>{
 });
 
 /*Obtiene los datos del usuario logueado*/
-router.get('/:phone', async(req, res) =>{
+router.get('/:phone', async (req, res) => {
   const phone = req.params.phone;
   try {
     const response = await User.findOne({ where: { phone } });
@@ -31,60 +32,68 @@ router.get('/:phone', async(req, res) =>{
 
 
 /* Actualiza los datos de usuario de la base de datos*/
-router.put('/update', async (req, res) =>{
+router.put('/update', async (req, res) => {
 
-  const {firstname, lastname, address, city, oldPhone, phone, email, password} = req.body
+  const { firstname, lastname, address, city, oldPhone, phone, email, password } = req.body
   console.log("Esto es password", password)
   //Validación que evalua si se envía una nueva contraseña
-  if(!password == ""){
+  if (!password == "") {
     //Se encripta la nueva contraseña
     const salt = 10;
     const data = bcrypt.hashSync(password, salt);
     console.log("Esto es password cifrado", data)
-      
+
     try {
       await User.update(
-        {phone: phone, firstname: firstname, lastname: lastname,
-         address: address, cityId: city, email: email, password: data },
-        {where:{
-          phone: oldPhone
-        }});
-        res.send(true);
-        /*try {
-          await Customer.update(
-            {phone: phone},
-            {where: {
-              phone: oldPhone
-            }});
-            res.send(true);
-        } catch (error) {
-          console.error(error)
-          res.send(false);
-        }*/
+        {
+          phone: phone, firstname: firstname, lastname: lastname,
+          address: address, cityId: city, email: email, password: data
+        },
+        {
+          where: {
+            phone: oldPhone
+          }
+        });
+      res.send(true);
+      /*try {
+        await Customer.update(
+          {phone: phone},
+          {where: {
+            phone: oldPhone
+          }});
+          res.send(true);
+      } catch (error) {
+        console.error(error)
+        res.send(false);
+      }*/
     } catch (error) {
       console.error(error);
       res.send(false);
     }
-  }else{
+  } else {
     try {
       await User.update(
-        {phone: phone, firstname: firstname, lastname: lastname,
-         address: address, cityId: city, email: email },
-        {where:{
-          phone: oldPhone
-        }});
-        res.send(true);
-        /*try {
-          await Customer.update(
-            {phone: phone},
-            {where: {
-              phone: oldPhone
-            }});
-            res.send(true);
-        } catch (error) {
-          console.error(error)
-          res.send(false);
-        }*/
+        {
+          phone: phone, firstname: firstname, lastname: lastname,
+          address: address, cityId: city, email: email
+        },
+        {
+          where: {
+            phone: oldPhone
+          }
+        });
+      res.send(true);
+      /*try {
+        await Customer.update(
+          {phone: phone},
+          {where: {
+            phone: oldPhone
+          }});
+          res.send(true);
+      } catch (error) {
+        console.error(error)
+        res.send(false);
+      }*/
     } catch (error) {
       console.error(error);
       res.send(false);
@@ -92,5 +101,12 @@ router.put('/update', async (req, res) =>{
   }
 
 })
+
+/* GET users listing. */
+router.get('/profile', async function (req, res, next) {
+  const result = await User.findOne({ where: { phone: req.user.phone } })
+  console.log(await result.getCityId());
+  res.status(200).json(result);
+});
 
 module.exports = router;

0d50516edcb770bad1403281e7229b3aa13235a2
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2023-06-20 21:25:05 -0500

    fix key error

--- src/pages/NewRequest.jsx
+++ src/pages/NewRequest.jsx
@@ -1,24 +1,24 @@
-import Request from '../sections/NewRequest'
 import Navbar from '../sections/NavbarMensajeros'
 import '../styles/NewRequest.css'
 import { useFetchData } from '../hooks/consumer'
-import {API_AllRequest } from '../hooks/request'
+import { API_AllRequest } from '../hooks/request'
 import { Skeleton } from '@mui/material'
-export default function NewRuquest() {
+import { Fragment } from 'react'
+import ItemRequest from '../sections/ItemRequest'
+
+export default function NewRequest() {
     const [data, loading] = useFetchData(API_AllRequest)
     // const [datos] = useFetchData(API_AllDelyvery)
     return (
-        <div>
+        <Fragment>
             <Navbar />
-            {loading ? <Skeleton variant="rectangular" width={210} height={118} /> :
-                <div className='request'>
-
-                    {data.map((data) => (
-                        <Request  key='{data.id}'data={data}/>
-                      
-                    ))}
-                </div>
+            {
+                loading
+                    ? <Skeleton variant="rectangular" width={210} height={118} />
+                    : <div className='request'>
+                        {data.map((data) => (<ItemRequest key={data.code} data={data} />))}
+                    </div>
             }
-        </div>
+        </Fragment>
     )
 }

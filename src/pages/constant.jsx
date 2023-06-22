a7fcc16a3b4dc28968de87ca21fb3f5ea18786e3
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2023-06-21 19:33:43 -0500

    añadir campos de solicitud

--- src/pages/constant.jsx
+++ src/pages/constant.jsx
@@ -20,6 +20,13 @@ const CreateFields = {
     { label: "Departamento", id: "stateId", type: 'select', rules: { required: true } },
     { label: "Ciudad", id: "cityId", type: 'select', dependsOn: "stateId", rules: { required: true } },
   ],
+  request: [
+    { label: "Descripcion del pedido", id: "description", rules: { required: true } },
+    { label: "Origen", id: "origin", rules: { required: true } },
+    { label: "Destino", id: "destination", rules: { required: true } },
+    { label: "Numero de paquetes", id: "numberPackages", type: 'numeric', rules: { required: true, pattern: /^\d+$/ }, helpers: { pattern: 'Solo se permiten numeros' } },
+    { label: "Tipo de transporte", id: "transportId", type: 'select', rules: { required: true } },
+  ]
 }
 
 const InputTypes = {

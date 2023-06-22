0171f3f2a3667125c30956f6525370b872a63ed5
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2023-06-21 19:43:33 -0500

    añadir una ruta general para transportes

--- routes/general.js
+++ routes/general.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const { City, State } = require('../db/Models');
+const { City, State, Transport } = require('../db/Models');
 var router = express.Router();
 
 router.get('/cities/:stateId', async function (req, res) {
@@ -17,4 +17,10 @@ router.get('/states', async function (req, res) {
     const r = await State.findAll({ attributes: [['id', 'value'], ['name', 'label']] })
     res.status(200).json(r);
 });
+
+router.get('/transport', async function (req, res) {
+    const r = await Transport.findAll({ attributes: [['id', 'value'], ['nameTransport', 'label']] })
+    res.status(200).json(r);
+});
+
 module.exports = router;

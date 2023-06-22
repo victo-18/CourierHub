42a42beb07ab79b9bd6454fc01066c84df48153a
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2023-06-21 19:44:05 -0500

    añadir ruta para nuevas solicitudes y cambios menores

--- routes/request.js
+++ routes/request.js
@@ -1,21 +1,34 @@
 var express = require('express');
-const { Request, ListState, User, Customer, sequelize } = require('../db/Models');
+const { Request, ListState, User, Customer, sequelize, Travel, Transport } = require('../db/Models');
 var router = express.Router();
 
 router.get('/', async function (req, res) {
     const result = await Request.findAll({
-        attributes: ["code", "destination"],
+        attributes: ["code", "destination", "numberPackages"],
         include: [
             { model: ListState, attributes: ["date", "phase", "image"], order: [["date", "DESC"]], limit: 1, separate: true },
-            { attributes: ["id"], model: Customer, include: { model: User, attributes: ["phone", "firstname", "lastname", "address", "email"] } }],
+            { model: Transport, attributes: ["id", "nameTransport"] },
+            { model: User, attributes: ["phone", "firstname", "lastname", "address", "email"] }
+        ],
     });
     res.status(200).json(result);
+});
+
+router.post('/', async function (req, res) {
+    const { transportId, ...others } = req.body;
+
+    const customer = await Customer.findOne({ where: { phone: req.user.phone } })
+    const request = await Request.create({ ...others, customerId: customer.dataValues.id });
+    await Travel.create({ requestCode: request.dataValues.code, transportId });
+    await ListState.create({ requestCode: request.dataValues.code });
+
+    res.status(200).json(request);
 });
 
 // => hostname/api/v1/request/inProgress
 router.get('/inProgress', async function (req, res) {
     const result = await Request.findAll({
-        attributes: ["code", "destination","description"],
+        attributes: ["code", "destination", "description"],
         include: [{
             model: ListState,
             attributes: ["date", "phase", "image"],

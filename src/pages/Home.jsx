4e070b28027460b446e2548de03078fd92ed2f89
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2023-06-21 19:33:52 -0500

    añadir appbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
-import { Skeleton } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import { Backdrop, CircularProgress, Fab } from "@mui/material";
 import Box from "@mui/material/Box";
 import { Helmet } from "react-helmet-async";
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
 import { CustomList } from "../components/LIst/CustomList";
+import HideAppBar from "../components/Navbar";
 import { useFetchData } from "../hooks/consumer";
 import { API_AllRequest } from "../hooks/request";
-import HideAppBar from "../components/Navbar";
 
 export function Home() {
     const { auth } = useSelector((state) => state.session);
@@ -26,10 +27,18 @@ export function Home() {
             <HideAppBar>
                 {
                     loadingShipments ?
-                        <Skeleton />
+                        <Backdrop open sx={{ zIndex: 1, bgcolor: 'background.paper' }}>
+                            <CircularProgress />
+                        </Backdrop>
                         : <CustomList data={shipments} />
                 }
             </HideAppBar>
+            <Box sx={{ position: "fixed", right: 16, bottom: 16 }}>
+                <Fab LinkComponent={Link} color="primary" to="nuevo">
+                    <AddIcon />
+                </Fab>
+            </Box>
+            <Outlet />
         </Box>)
         :
         (<Navigate to="/login" />)

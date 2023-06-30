51b407873aeff2c9da034a2923e163d6e6425ff1
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2023-06-30 14:57:55 -0500

    añadir curl

--- build.sh
+++ build.sh
@@ -9,3 +9,5 @@ cd ../server-courierhub/
 git add dist/
 git commit -m "build $(date +%s) ($(date +"%Y-%m-%d %H:%M:%S"))"
 git push origin server
+
+curl -X GET https://api.render.com/deploy/srv-cif6j195rnujc4ph1hr0?key=HEaYw4dk_JE

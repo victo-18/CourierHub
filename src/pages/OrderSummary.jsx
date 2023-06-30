ca744a5754f5dab47d396f4cf398ba02c8e4b542
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2023-06-30 13:43:01 -0500

    añadir navbar

--- src/pages/OrderSummary.jsx
+++ src/pages/OrderSummary.jsx
@@ -1,18 +1,18 @@
-import NabarMensajeros from "../sections/NabarMensajeros"
-import SummaryRequest from "../components/SummaryRequest"
-import { Typography } from "@mui/material"
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-import "../styles/OrderSummary.css"
+import HideAppBar from "../components/Navbar";
+import SummaryRequest from "../components/SummaryRequest";
+import "../styles/OrderSummary.css";
 
 export default function OrderSummary() {
     const { auth } = useSelector((state) => state.session);
     return auth ? (
         <div id="mainContainer">
-            <NabarMensajeros />
-            <div id="component">
-            <SummaryRequest />
-            </div>
+            <HideAppBar>
+                <div id="component">
+                    <SummaryRequest />
+                </div>
+            </HideAppBar>
         </div>
     ) : (<Navigate to="/dashboard" />)
 }

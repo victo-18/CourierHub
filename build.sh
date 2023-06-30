c9018d54539c77cec1922836b112a9213c9da7c6
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2023-06-30 00:46:36 -0500

    añadir auto-deploy

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #bin/bash/
+export GCM_CREDENTIAL_STORE=secretservice
 
 rm -r ../server-courierhub/dist/
 cp -r ./dist/ ../server-courierhub/dist/
+
+cd ../server-courierhub/
+git add dist/
+git commit -m "build $(date +%s) ($(date +"%Y-%m-%d %H:%M:%S"))"
+git push origin server

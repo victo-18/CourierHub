7bb4def40bbbc3cc79bff34def98542cf33a6b88
JuanSe320 <juansecamino96@gmail.com>
2023-06-30 13:17:10 -0500

    ingreso de las paginas delivery y cambio de navbar

--- src/pages/Entregas.jsx
+++ src/pages/Entregas.jsx
@@ -1,4 +1,5 @@
 import NabarMensajeros from "../sections/NabarMensajeros"
+import Navbar from "../components/Navbar"
 //import DeliveryDay from "../sections/DeliveryDay"
 import { Box, Stack } from "@mui/material"
 import {DeliveryTabs} from "../components/DeliveryTabs"
@@ -6,7 +7,7 @@ import "../styles/Entrega.css"
 export default function Entregas() {
     return (
         <Box>
-            <NabarMensajeros />
+            <Navbar />
             <DeliveryTabs/>
         </Box>
     )

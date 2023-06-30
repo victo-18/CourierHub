fe50483259128f149f366ebcbbcdc8f06c9db396
johanruizb <johan.andres.ruiz@correounivalle.edu.co>
2023-06-30 14:33:59 -0500

    build 1688153638 (2023-06-30 14:33:58)

--- dist/assets/vendor_react-router-dom-ed3b9d4c.js
+++ dist/assets/vendor_react-router-dom-ed3b9d4c.js
@@ -1,4 +1,4 @@
-import{as as _,at as C,au as x,av as S,r as h,aw as O,ax as P,ay as B,az as j,aA as z,aB as D,aC as R}from"./vendor-cc9b532d.js";/**
+import{as as _,at as C,au as x,av as S,r as h,aw as O,ax as P,ay as B,az as j,aA as z,aB as D,aC as R}from"./vendor-b3c2ed37.js";/**
  * React Router DOM v6.13.0
  *
  * Copyright (c) Remix Software Inc.
